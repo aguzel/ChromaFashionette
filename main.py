61642ba8da68aae86680db8e2100015afdffc734
aguzel <ahmethamdiguzel@gmail.com>
2023-02-12 03:33:53 +0000

    training is fixed, decoding migrated to torch

--- main.py
+++ main.py
@@ -6,112 +6,100 @@ import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
-from unet import UnetGenerator
-from fcn import *
+from models.unet import UnetGenerator
+from models.fcn import *
+from utils import *
+import torch.utils.tensorboard as tb
+writer = tb.SummaryWriter('runs/fcn32s-v1')
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-train_image_dir = "/data"
-transform = transforms.Compose([transforms.ToTensor()])
-dataset = FashionImageSegmentationDataset(root_dir='data', mode='train', transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
+BATCH_SIZE = 8
 
+transform = transforms.Compose([transforms.ToTensor()])
+trainset = FashionImageSegmentationDataset(root_dir='data', mode='train', transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+
+testset = FashionImageSegmentationDataset(root_dir='data', mode='test', transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
 # %%
 # Display the first batch of images
-images, labels, gt_images = next(iter(dataloader))
-grid = torchvision.utils.make_grid(images, nrow=8)
-plt.figure(figsize=(15, 15))
-plt.imshow(grid.numpy().transpose((1, 2, 0)))
-plt.axis('off')
-plt.show()
+images, labels, gt_images = next(iter(trainloader))
+show_grid_images(images, nrow=8)
+show_grid_images(gt_images, nrow=8)
+show_grid_labels(labels, nrow=8)
+print(labels.shape)
 
-grid = torchvision.utils.make_grid(gt_images, nrow=8)
-plt.figure(figsize=(15, 15))
-plt.imshow(grid.numpy().transpose((1, 2, 0)))
-plt.axis('off')
-plt.show()
-
-gt_label = labels.unsqueeze(1)
-grid = torchvision.utils.make_grid(gt_label, nrow=8)
-plt.figure(figsize=(15, 15))
-print(grid.shape)
-plt.imshow(grid[0], cmap='gray')
-plt.axis('off')
-plt.show()
 
 # %% create network
-# net = UnetGenerator(3, 5, 7, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False)
-# net = net.to(device)
-# print(net)
-
-vgg_model = VGGNet(requires_grad=True, remove_fc=True)
-net = FCNs(pretrained_net=vgg_model, n_class=5)
+net = UnetGenerator(3, 5, 7, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False)
 net = net.to(device)
 print(net)
-# %%
+#%%
+# vgg_model = VGGNet(requires_grad=True, remove_fc=True)
+# net = FCN32s(pretrained_net=vgg_model, n_class=5)
+# net = net.to(device)
+
+
 loss_func = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(),
                              lr=0.0002,
                              betas=(0.9, 0.999),
                              eps=1e-08,
                              weight_decay=0,
-                             )
-                             
+                             )                         
 
 # %%
-epoch = 5
+epochs = 20
 net.train()
-for epoch in range(epoch):
-    running_loss = 0.0
-    for inputs, targets, _ in dataloader:
+for epoch in range(epochs):
+    training_loss = 0.0
+    test_loss = 0.0
+    for i, data in enumerate(trainloader):
+        inputs, targets, _ = data
         optimizer.zero_grad()
-
-        inputs = images.to(device)
-        targets = labels.to(device)        
+        inputs = inputs.to(device)
+        targets = targets.to(device)        
         predictions = net(inputs)        
-        loss = loss_func(predictions, targets)
-        
+        loss = loss_func(predictions, targets)        
         loss.backward(retain_graph=True)
         optimizer.step()
+        training_loss += loss.item()
+        if i % 160 == 159:    
+          writer.add_scalar('training loss',
+                          training_loss / 160,
+                          epoch * len(trainloader) + i) 
 
-        running_loss += loss.item()
-    print('[Epoch %d/%d] Loss: %.4f' % (epoch + 1, epoch, running_loss / len(dataloader)))
-torch.save(net.state_dict(), "fcn_vgg_model.pt")
-
-# %%
-plt.imshow(images[7].detach().cpu().numpy().transpose(1,2,0))
-# %%
-plt.imshow(gt_images[7].detach().cpu().numpy().transpose(1,2,0))
-# %%
-# plt.imshow(labels[7], cmap='gray')
-example_result  = (net(images[7].unsqueeze(0).to(device)))
-out_1 = torch.argmax(example_result.squeeze(), dim=0).detach().cpu().numpy() 
-
-# %%
-print(out_1.shape)
-plt.imshow(out_1)
-plt.savefig('out.png')
-# %%
-# Define the helper function
-def decode_segmap(image, nc=5):
-  label_colors = np.array([(0, 0, 0),
-                           (66, 127, 50),
-                           (224, 16, 115),
-                           (250, 190, 8),
-                           (147, 225, 245)])
-
-  r = np.zeros_like(image).astype(np.uint8)
-  g = np.zeros_like(image).astype(np.uint8)
-  b = np.zeros_like(image).astype(np.uint8)
-  for l in range(0, nc):
-    idx = image == l
-    r[idx] = label_colors[l, 0]
-    g[idx] = label_colors[l, 1]
-    b[idx] = label_colors[l, 2]
-  rgb = np.stack([r, g, b], axis=2)
-  return rgb
-
-rgb = decode_segmap(out_1)
-plt.imshow(rgb)
+    with torch.no_grad():
+        for i, data in enumerate(testloader):
+            inputs, targets, gt_rgb = data
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            predictions = net(inputs)
+            loss = loss_func(predictions, targets)
+            test_loss += loss.item()
+            if i % 40 == 39:    
+              writer.add_scalar('test loss',
+                              test_loss / 40,
+                              epoch * len(testloader) + i) 
+     
+        print('[Epoch %d/%d] Training Loss: %.4f Test Loss: %.4f' % (epoch + 1, epochs, training_loss / len(trainloader),
+                                                                                        test_loss / len(testloader)))
+PATH = "trained_fcn8s.pt"
+torch.save(net.state_dict(), PATH )
+print("The End of Training and model saved to {}".format(PATH))
+#%%
+net.load_state_dict(torch.load("trained_fcn8s.pt"))
+net.eval()
+#%%
+testloader = torch.utils.data.DataLoader(testset, batch_size=2, shuffle=True, num_workers=4)
+images, labels, gt_images = next(iter(testloader))
+show_grid_images(images.detach().cpu())
+show_grid_images(gt_images.detach().cpu())
+example_results = (net(images.to(device)))
+preds_ = torch.argmax(example_results.squeeze(), dim=1)
+output_images = decode_output(preds_)
+show_grid_images(output_images.detach().cpu())
+print(pixel_accuracy(decode_output(preds_).detach().cpu(),gt_images ))
---
+++ utils.py
@@ -0,0 +1,73 @@
+import torchvision
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+
+def show_grid_images(images, nrow = 8):
+    grid = torchvision.utils.make_grid(images, nrow)
+    plt.figure(figsize=(15, 15))
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+    return None
+
+def show_grid_labels(labels, nrow = 8):
+    gt_label = labels.unsqueeze(1)
+    grid = torchvision.utils.make_grid(gt_label, nrow)
+    plt.figure(figsize=(15, 15))
+    plt.imshow(grid[0], cmap='gray')
+    plt.axis('off')
+    plt.show()
+    return None
+
+def decode_output(images, nc=5):
+  label_colors = torch.tensor([(0, 0, 0),
+                           (66, 127, 50),
+                           (224, 16, 115),
+                           (250, 190, 8),
+                           (147, 225, 245)])
+  image_list =[]
+  rgb_batch = torch.zeros(images.shape[0], 3, images.shape[1], images.shape[2])
+  for i in range(images.shape[0]):
+    r = torch.zeros_like(images[i]).to(torch.uint8)
+    g = torch.zeros_like(images[i]).to(torch.uint8)
+    b = torch.zeros_like(images[i]).to(torch.uint8)
+    for l in range(0, nc):
+        idx = images[i] == l
+        r[idx] = label_colors[l, 0]
+        g[idx] = label_colors[l, 1]
+        b[idx] = label_colors[l, 2]
+    rgb = torch.stack([r, g, b], dim=0)
+    image_list.append(rgb)
+    rgb_batch = torch.stack(image_list)
+  return rgb_batch
+
+
+def pixel_accuracy(pred, target):
+    """
+    Calculates the pixel accuracy between two RGB images.
+    
+    Arguments:
+        pred: PyTorch tensor, shape (N, 3, H, W), the predicted image.
+        target: PyTorch tensor, shape (N, 3, H, W), the ground truth image.
+        
+    Returns:
+        accuracy: float, the pixel accuracy.
+    """
+    # Convert the RGB images to grayscale
+    pred = 0.2989 * pred[:, 0, :, :] + 0.5870 * pred[:, 1, :, :] + 0.1140 * pred[:, 2, :, :]
+    target = 0.2989 * target[:, 0, :, :] + 0.5870 * target[:, 1, :, :] + 0.1140 * target[:, 2, :, :]
+    
+    # Convert the images to binary masks by thresholding
+    
+    # Calculate the number of correctly classified pixels
+    correct = (pred == target).sum().item()
+    
+    # Calculate the total number of pixels
+    total = pred.numel()
+    
+    accuracy = correct / total
+    
+    return accuracy
+
+
